b69d40deecd544d612b28022278c1766c59f89f5
duriyodbek <tduriedbek@gmail.com>
2025-02-24 11:53:55 +0500

    done

---
+++ 24-02/index.js
@@ -0,0 +1,16 @@
+console.log("hey")
+
+function allowed (param){
+    if (param>=18) {
+        console.log("Allowed: " + param + "18dan katta yoki teng");
+    } else{
+        console.log("not allowed: " + param + "18 dan kichik bolsa")
+    } 
+}
+
+allowed(19) 
+
+
+function logtitle(param){
+    
+}
